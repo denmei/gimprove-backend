b019442606e8e99d52b6e77eca72974483da7c42
dennis <meisnerdennis@web.de>
2017-12-12 15:55:48 +0100

    heroku

--- smartgym/settings.py
+++ smartgym/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mua*5%xiv5z6s27si3am&&m+o=@gg#$f#gv^8j-i%h+kc6$8jt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
