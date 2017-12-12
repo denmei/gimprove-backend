e9a7954f8c5f9c4baf8479a96f0b5419acb62710
dennis <meisnerdennis@web.de>
2017-12-12 16:18:21 +0100

    heroku

--- smartgym/settings.py
+++ smartgym/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    'tracker/static',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
