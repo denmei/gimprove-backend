c17b720d9fc9de0b2304970021ae5a99973c2ec5
dennis <meisnerdennis@web.de>
2018-01-28 15:40:50 +0100

    RFID-Field

--- tracker/models.py
+++ tracker/models.py
@@ -67,6 +67,7 @@ class UserProfile(Profile):
     """
     date_of_birth = models.DateField(null=False, blank=False)
     gym = models.ManyToManyField('GymProfile', blank=True)
+    rfid_tag = models.CharField('RFID', max_length=10, blank=True, null=True, unique=True)
     achievements = models.ManyToManyField('Achievement', blank=True)
 
 
@@ -123,7 +124,7 @@ class TrainUnit(models.Model, LoginRequiredMixin):
     start_time_date = models.DateTimeField(null=False, blank=False)
     end_time_date = models.DateTimeField(null=False, blank=False)
     date = models.DateField(null=False, blank=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     exercise_units = models.ManyToManyField('ExerciseUnit', related_name='+', blank=True)
 
     def __str__(self):
@@ -155,6 +156,16 @@ class Set(models.Model):
     exercise_unit = models.ForeignKey(ExerciseUnit, on_delete=models.CASCADE)
     repetitions = models.IntegerField(blank=False)
     weight = models.IntegerField(blank=False)
+    rfid = models.CharField(blank=False, null=False, max_length=10)
+
+    """
+    @classmethod
+    def create(cls, repetitions, weight, rfid, exercise_unit=None, date_time=datetime.now()):
+        # TODO get exercise_unit for User or create new
+        if exercise_unit = None:
+            exercise_unit = 1
+        set = cls(repetitions=repetitions, weight=weight, exercise_unit=exercise_unit, date_time=date_time)
+    """
 
     def __str__(self):
         return str(self.id)
--- tracker/serializers.py
+++ tracker/serializers.py
@@ -6,5 +6,10 @@ class SetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Set
-        fields = ('id', 'date_time', 'exercise_unit', 'repetitions', 'weight')
+        fields = ('id', 'date_time', 'exercise_unit', 'repetitions', 'weight', 'rfid')
 
+    """def create(self, validated_data):
+        # exercise_unit = validated_data.pop('exercise_unit')
+        obj = Set.objects.create(**validated_data)
+        obj.save(exercise_unit=validated_data['exercise_unit'])
+        return obj"""
