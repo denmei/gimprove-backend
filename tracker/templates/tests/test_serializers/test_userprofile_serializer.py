e76315b6de1d83b9947b19a5c47276f5877a9147
dennis <meisnerdennis@web.de>
2018-02-18 13:20:06 +0100

    UserProfileSerializer in own .py file. Test cases adapated.

--- tracker/templates/tests/test_serializers/test_userprofile_serializer.py
+++ tracker/templates/tests/test_serializers/test_userprofile_serializer.py
@@ -17,6 +17,9 @@ class UserProfileSerializerTest(APITestCase):
         self.active_set = UserProfile.objects.all()[0].active_set
 
     def test_userprofile_retrieval(self):
+        """
+        Tests whether a UserProfile can be retrieved properly by the User-Id.
+        """
         response = self.c.get(self.pre_http + reverse('userprofile_detail', kwargs={'pk': self.user.id}))
         content = (json.loads(response.content.decode("utf-8")))
         self.assertEqual(response.status_code, 200)
@@ -25,28 +28,15 @@ class UserProfileSerializerTest(APITestCase):
         else:
             self.assertEqual(content['active_set'], str(self.active_set))
 
-
-class UserProfileRfidSerializerTest(APITestCase):
-    """
-    Tests the functionality of the UserSerializer (by RFID-Tag).
-    """
-
-    fixtures = ['fix.json']
-
-    def setUp(self):
-        self.c = RequestsClient()
-        self.pre_http = "http://127.0.0.1:8000"
-        self.rfid_tag = UserProfile.objects.all()[0].rfid_tag
-        self.user = UserProfile.objects.all()[0]
-        self.active_set = UserProfile.objects.all()[0].active_set
-        self.user_id = self.user.user.id
-
-    def test_userprofile_retrieval(self):
-        response = self.c.get(self.pre_http + reverse('userprofile_rfid_detail', kwargs={'rfid_tag': self.user.rfid_tag}))
+    def test_userprofile_retrieval_rfid(self):
+        """
+        Tests whether a UserProfile can be retrieved properly by the User-RFID-Number.
+        """
+        response = self.c.get(self.pre_http + reverse('userprofile_rfid_detail', kwargs={'rfid_tag': self.rfid_tag}))
         content = (json.loads(response.content.decode("utf-8")))
         self.assertEqual(response.status_code, 200)
         if content['active_set'] is None:
             self.assertEqual(content['active_set'], self.active_set)
         else:
             self.assertEqual(content['active_set'], str(self.active_set))
-        self.assertEqual(content['user'], self.user_id)
+        self.assertEqual(content['user'], self.user.id)
