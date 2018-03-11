f430233a1f8b7d8c96213a819ff6b8c30341acd4
dennis <meisnerdennis@web.de>
2018-03-11 11:46:42 +0100

    main.js

--- tracker/static/tracker/js/app_mockup/main.js
+++ tracker/static/tracker/js/app_mockup/main.js
@@ -8,12 +8,15 @@ $(document).ready(function () {
     // TODO: change when deploying to heroku
     var link_set_detail = 'https://app-smartgym.herokuapp.com/tracker/set_detail_rest/';
     var link_userprofile_detail = 'https://app-smartgym.herokuapp.com/tracker/userprofile_detail_rest/';
-    // var link_set_detail = 'http://127.0.0.1:8000/tracker/set_detail_rest/';
-    // var link_userprofile_detail = 'http://127.0.0.1:8000/tracker/userprofile_detail_rest/';
+    //var link_set_detail = 'http://127.0.0.1:8000/tracker/set_detail_rest/';
+    //var link_userprofile_detail = 'http://127.0.0.1:8000/tracker/userprofile_detail_rest/';
     link_userprofile_detail = link_userprofile_detail + user_id;
     var active = false;
     var reps = 0;
 
+    if(navigator.userAgent.match(/Android/i)){
+    window.scrollTo(0,1);
+    }
 
     function loop() {
         setTimeout(function () {
