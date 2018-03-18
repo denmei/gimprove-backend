5ffb34fa17207d08d0d20af955217aa8e667671e
dennis <meisnerdennis@web.de>
2018-03-18 18:53:24 +0100

    staticfiles.

---
+++ staticfiles/tracker/js/user_activities.58196a8c769c.js
@@ -0,0 +1,66 @@
+/*
+AJAX for updating the User-Profile page quickly.
+*/
+
+$(document).ready(function () {
+
+    var i = 1;
+    var active = false;
+
+    var currentLocation = window.location;
+    if(currentLocation.toString().indexOf("127.0.0.1") > -1) {
+        var link_set_detail = 'http://127.0.0.1:8000/tracker/set_detail_rest/'
+        var link_userprofile_detail = 'http://127.0.0.1:8000/tracker/userprofile_detail_rest/'
+    } else {
+        var link_set_detail = 'https://app-smartgym.herokuapp.com/tracker/set_detail_rest/';
+        var link_userprofile_detail = 'https://app-smartgym.herokuapp.com/tracker/userprofile_detail_rest/';
+    };
+
+    link_userprofile_detail = link_userprofile_detail + user_id;
+
+    function loop() {
+        setTimeout(function () {
+            // check whether there is an active set. If so, show set data.
+            $.ajax({
+                type: 'GET',
+                url: link_userprofile_detail,
+                success: function(response_set) {
+                    if(response_set.active_set) {
+                        // has active set:
+                        if(active == false) {
+                            $('#active_set_div').show();
+                            active = true;
+                        };
+                        // if active set, get data and update
+
+                        $.ajax({
+                            type: 'GET',
+                            url: link_set_detail + response_set.active_set,
+                            success: function(response_set) {
+                                $('#exercise_name').text("Active Workout: " + ex_name.toString());
+                                $('#weight').text(response_set.weight.toString());
+                                $('#repetitions').text(response_set.repetitions.toString());
+                            }
+                        });
+
+                    } else {
+                    // no active set:
+                        if(active == true) {
+                            $('#active_set_div').hide();
+                            active = false;
+                        };
+                    }
+                }
+            });
+
+            i ++;
+
+            if(true) {
+                loop();
+            };
+        }, 1000)
+    };
+
+    loop();
+
+});
