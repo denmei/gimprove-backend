7c961ceb457122563a0073c5b0685b3cf82303d4
dennis <meisnerdennis@web.de>
2018-05-02 21:45:02 +0200

    SetConsumer initialized.

--- smartgym/routing.py
+++ smartgym/routing.py
@@ -3,12 +3,13 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 import tracker.routing
 from django.urls import path
 from tracker.consumers.SetConsumer import SetConsumer
+from django.conf.urls import url
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter([
-            path("ws/tracker", SetConsumer),
+            url(tracker.routing.websocket_urlpatterns, SetConsumer)
         ])
     ),
 })
--- tracker/consumers/SetConsumer.py
+++ tracker/consumers/SetConsumer.py
@@ -1,15 +1,37 @@
 from channels.generic.websocket import WebsocketConsumer
+from tracker.models.models import UserProfile
 import json
 
 
 class SetConsumer(WebsocketConsumer):
+
     def connect(self):
         self.accept()
-        self.send("Connected!")
-        print("Connected")
+        self.user = self.scope['user']
+        self.anonymousUser = (self.user == "AnonymousUser" or self.user is None)
+        if not self.anonymousUser:
+            self.user_profile = self.__initialize_user__(self.user)
 
     def disconnect(self, code):
         print("Disconnected")
 
     def receive(self, text_data=None, bytes_data=None):
-        print(text_data)
+        message = json.loads(text_data)
+        if self.__message_valid__(message) and not self.anonymousUser:
+            print("Valid")
+        else:
+            self.send("Invalid message. Disconnect.")
+            self.disconnect(400)
+
+    def __initialize_user__(self, user):
+        profile = UserProfile.objects.get(user=user)
+        return profile
+
+    def __message_valid__(self, message):
+        """
+        Checks whether a message has the appropriate format.
+        :param message: Message to be checked.
+        :return: True if format is OK, False otherwise.
+        """
+        # TODO
+        return True
